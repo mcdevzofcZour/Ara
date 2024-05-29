0df8dafc1f973327fc4820341a7c3a3993a2ebcb
HeightHazard <rohmanhabibu277@gmail.com>
2024-05-29 21:01:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.MONGO_DB_URI = "mongodb+srv://rohmanhabibu277:habib898@cluster0.jsebaub.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "habib898" //isi apa aja bebas
+global.your_email = "rohmanhabibu277@gmail.com" //email
+global.email_password = "ybjd hbvz meju pwti" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
